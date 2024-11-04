2f53647135226fde6dcf7111ac01b0a30d68e9e1
Programistich <dzhos.oleksii@gmail.com>
2024-11-04 21:48:00 +0200

    [System Design] Fonts

--- iOS/Components/Font.swift
+++ iOS/Components/Font.swift
@@ -8,4 +8,44 @@ extension Font {
     static func ppNeueMontrealRegular(size: Double) -> Font {
         .custom("PPNeueMontreal-Regular", size: size)
     }
+
+    static func ppNeueMontrealMedium(size: Double) -> Font {
+        .custom("PPNeueMontreal-Medium", size: size)
+    }
+
+    static func ppNeueMontrealSemiBold(size: Double) -> Font {
+        .custom("PPNeueMontreal-SemiBold", size: size)
+    }
+
+    static var titlePrimary: Font {
+        .ppNeueMontrealMedium(size: 24)
+    }
+
+    static var titleSecondary: Font {
+        .ppNeueMontrealMedium(size: 20)
+    }
+
+    static var bodyPrimary: Font {
+        .ppNeueMontrealRegular(size: 16)
+    }
+
+    static var bodySecondary: Font {
+        .ppNeueMontrealRegular(size: 14)
+    }
+
+    static var labelPrimary: Font {
+        .ppNeueMontrealMedium(size: 16)
+    }
+
+    static var labelSecondary: Font {
+        .ppNeueMontrealMedium(size: 12)
+    }
+
+    static var labelLink: Font {
+        .ppNeueMontrealMedium(size: 16)
+    }
+
+    static var labelBold: Font {
+        .ppNeueMontrealSemiBold(size: 16)
+    }
 }
