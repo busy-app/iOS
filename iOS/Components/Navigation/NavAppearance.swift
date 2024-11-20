82b07aa314536d70c780bc5b53b888473db1bdb3
tonyfreeman <24860049+tonyfreeman@users.noreply.github.com>
2024-11-20 21:53:57 +0300

    Not toolbar customization

--- iOS/Components/Navigation/NavAppearance.swift
+++ iOS/Components/Navigation/NavAppearance.swift
@@ -4,8 +4,6 @@ extension View {
     // NOTE: Not sure about this modifier, but it's used with NavigationStack
     func navAppearance() -> some View {
         self
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.surfacePrimary)
             .navigationBarBackButtonHidden(true)
             .navigationBarTitleDisplayMode(.inline)
     }
