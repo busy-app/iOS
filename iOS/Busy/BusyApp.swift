94e615e3b960e823ae7871f5ee9d0ed37dda6fad
tonyfreeman <24860049+tonyfreeman@users.noreply.github.com>
2024-11-27 00:19:37 +0300

    [UI] Setup timer view

--- iOS/Busy/BusyApp.swift
+++ iOS/Busy/BusyApp.swift
@@ -11,37 +11,93 @@ struct BusyApp: View {
         authorizationCenter.authorizationStatus == .approved
     }
 
-    @State var isOnToggle: Bool = false
     @AppStorage("isOn") var isOn: Bool = false
 
+    @AppStorage("minute") var minute: Int = 15
+    @AppStorage("second") var second: Int = 0
+
+    @State var timer = Timer()
+
+    var background: Color {
+        isOn ? .backgroundBusy : .backgroundDefault
+    }
+
     var body: some View {
-        VStack(spacing: 40) {
-            Toggle(isOn: $isOnToggle) {
-                Text("Turn \(isOnToggle ? "off" : "on")")
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                background
+                    .frame(maxWidth: .infinity)
+                    .frame(height: proxy.safeAreaInsets.top)
+                    .clipShape(
+                        .rect(
+                            topLeadingRadius: 0,
+                            bottomLeadingRadius: 16,
+                            bottomTrailingRadius: 16,
+                            topTrailingRadius: 0
+                        )
+                    )
+                    .padding(.bottom, 2)
+
+                VStack(spacing: 0) {
+                    ZStack {
+                        TimePicker(
+                            minute: $minute,
+                            second: $second
+                        )
+                    }
+                    .font(.pragmaticaNextVF(size: 100))
+                    .frame(maxHeight: .infinity)
+                    .minimumScaleFactor(0.1)
+
+                    Spacer(minLength: 0)
+
+                    BusyButton(isOn: isOn) {
+                        guard minute > 0 || second > 0 else {
+                            return
+                        }
+                        guard isAuthorized else {
+                            authorize()
+                            return
+                        }
+                        !isOn ? startBusy() : stopBusy()
+                    }
+                    .padding(.top, 16)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 128)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(background)
+                .clipShape(
+                    .rect(
+                        topLeadingRadius: 16,
+                        bottomLeadingRadius: 0,
+                        bottomTrailingRadius: 0,
+                        topTrailingRadius: 16
+                    )
+                )
             }
-            .onChange(of: isOnToggle) { newValue in
-                guard newValue != isOn else { return }
-
-                print("Turining \(newValue ? "on" : "off")")
-
-                guard isAuthorized else {
-                    isOnToggle = isOn
+            .ignoresSafeArea(.all)
+            .background(.blackOnContent)
+            .onChange(of: timer.state) { old, new in
+                if new == .stopped, isOn {
+                    stopBusy()
+                }
+            }
+            .task {
+                if !isAuthorized {
                     authorize()
-                    return
+                }
+                if isOn {
+                    startBusy()
                 }
 
-                isOn = newValue
-                isOn ? enableShield() : disableShield()
-            }
-        }
-        .padding(.horizontal, 24)
-        .task {
-            isOnToggle = isOn
-            if !isAuthorized {
-                authorize()
-            }
-            if isOn {
-                enableShield()
+                NotificationCenter.default.addObserver(
+                    forName: UIApplication.willTerminateNotification,
+                    object: nil,
+                    queue: .main
+                ) { _ in
+                    stopBusy()
+                }
             }
         }
     }
@@ -58,6 +114,18 @@ struct BusyApp: View {
         }
     }
 
+    func startBusy() {
+        timer.start(minutes: minute, seconds: second)
+        enableShield()
+        isOn = true
+    }
+
+    func stopBusy() {
+        isOn = false
+        timer.stop()
+        disableShield()
+    }
+
     func enableShield() {
         managedSettingsStore.shield.applicationCategories = .all(except: [])
         managedSettingsStore.shield.webDomainCategories = .all(except: [])
@@ -69,4 +137,8 @@ struct BusyApp: View {
         managedSettingsStore.shield.webDomains = nil
         managedSettingsStore.shield.webDomainCategories = nil
     }
+}
+
+#Preview {
+    BusyApp()
 }
