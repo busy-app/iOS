d0ca600425b35bcacfff276be6571514a80a5366
tonyfreeman <24860049+tonyfreeman@users.noreply.github.com>
2024-11-27 00:09:28 +0300

    Simple Timer model

---
+++ iOS/Busy/Timer.swift
@@ -0,0 +1,79 @@
+import Observation
+
+@Observable
+class Timer {
+    var state: State = .stopped
+
+    var minutes: Int = 0
+    var seconds: Int = 0
+
+    enum State {
+        case running
+        case stopped
+        case paused
+    }
+
+    private var timer: Task<Void, Swift.Error>?
+
+    func start(minutes: Int, seconds: Int) {
+        if state != .stopped {
+            stop()
+        }
+        self.minutes = minutes
+        self.seconds = seconds
+        resume()
+    }
+
+    func pause() {
+        state = .paused
+        timer?.cancel()
+        timer = nil
+    }
+
+    func resume() {
+        state = .running
+        timer = Task {
+            while minutes > 0 || seconds > 0 {
+                try await Task.sleep(for: .seconds(1))
+                if seconds > 0 {
+                    seconds -= 1
+                } else if minutes > 0 {
+                    seconds = 59
+                    minutes -= 1
+                }
+            }
+            stop()
+        }
+    }
+
+    func stop() {
+        state = .stopped
+    }
+
+    func increase() {
+        seconds += 5
+
+        if seconds >= 60 {
+            if minutes < 99 {
+                minutes += 1
+                seconds = seconds % 60
+            } else {
+                seconds = 59
+            }
+        }
+    }
+
+    func decrease() {
+        seconds -= 5
+
+        if seconds < 0 {
+            if minutes > 0 {
+                minutes -= 1
+                seconds += 60
+            } else {
+                seconds = 0
+                stop()
+            }
+        }
+    }
+}
