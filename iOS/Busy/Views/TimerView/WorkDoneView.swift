af0bc48e1f3f7b34a587750855c20aa50e750352
Tony Freeman <tonyfreeman@users.noreply.github.com>
2025-02-26 12:35:47 +0300

    [UI] Add WorkDoneView

---
+++ iOS/Busy/Views/TimerView/WorkDoneView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+extension TimerView {
+    struct WorkDoneView: View {
+        @Environment(\.appState) var appState
+
+        var body: some View {
+            VStack(spacing: 0) {
+                Spacer()
+
+                Image(.checkmarkGreen)
+
+                Text("BUSY 1/3 done!")
+                    .font(.pragmaticaNextVF(size: 32))
+                    .foregroundStyle(.whiteInvert)
+                    .padding(.top, 12)
+
+                Text("It’s time to have a rest")
+                    .foregroundStyle(.transparentWhiteInvertPrimary)
+                    .padding(.top, 12)
+
+                Button {
+                    appState.wrappedValue = .resting
+                } label: {
+                    Text("Start rest")
+                        .font(.pragmaticaNextVF(size: 24))
+                        .foregroundStyle(.blackInvert)
+                        .frame(width: 202, height: 64)
+                        .background(.whiteInvert)
+                        .clipShape(RoundedButton())
+                        .padding(.top, 32)
+                }
+
+                Spacer()
+
+                Button {
+                    appState.wrappedValue = .finished
+                } label: {
+                    Text("Finish for today")
+                        .foregroundStyle(.transparentWhiteInvertPrimary)
+                        .font(.pragmaticaNextVF(size: 18))
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 60)
+                }
+            }
+            .padding(.top, 51)
+            .padding(.bottom, 8)
+            .background(.backgroundDark)
+        }
+    }
+}
+
+#Preview {
+    TimerView.WorkDoneView()
+        .colorScheme(.light)
+}
