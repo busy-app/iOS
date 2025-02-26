150094d4cc34dc20f8c8909de6a840b05f543847
Programistich <35292229+Programistich@users.noreply.github.com>
2025-02-26 16:11:31 +0200

    Shortcut for Start and Stop Busy

--- iOS/Busy/Intents.swift
+++ iOS/Busy/Intents.swift
@@ -15,3 +15,40 @@ struct StopBusyIntent: AppIntent {
         return .result()
     }
 }
+
+struct StartBusyIntent: LiveActivityIntent {
+    static var title: LocalizedStringResource { "Start Busy" }
+
+    static var openAppWhenRun: Bool { true }
+
+    @Parameter(
+        title: "Duration in Minutes",
+        requestValueDialog: "How many minutes?"
+    )
+    var durationMinutes: Int
+
+    @MainActor func perform() async throws -> some IntentResult {
+        print("Starting Busy for \(durationMinutes) minutes")
+        Timer.shared.start(minutes: durationMinutes, seconds: 0)
+        return .result()
+    }
+}
+
+struct BusyShortcuts: AppShortcutsProvider {
+    static var shortcutTileColor: ShortcutTileColor { .red }
+
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: StartBusyIntent(),
+            phrases: [ "Start \(.applicationName)" ],
+            shortTitle: "Start Busy",
+            systemImageName: "timer"
+        )
+        AppShortcut(
+            intent: StopBusyIntent(),
+            phrases: [ "Stop \(.applicationName)" ],
+            shortTitle: "Stop Busy",
+            systemImageName: "timer"
+        )
+    }
+}
