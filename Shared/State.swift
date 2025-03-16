c575d6e8f46341291a68e175a10b1827b21742a3
Tony Freeman <tonyfreeman@users.noreply.github.com>
2025-03-16 18:06:31 +0300

    Move state to shared folder

---
+++ Shared/State.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public enum TimerState: Codable, Sendable {
+    case paused
+    case running
+    case finished
+}
+
+public enum IntervalKind: Codable, Sendable {
+    case work
+    case rest
+    case longRest
+}
+
+public enum IntervalSequence: Codable, Sendable {
+    case infinite(Int)
+    case finite(current: Int, total: Int)
+}
+
+public struct IntervalState: Codable, Hashable, Sendable {
+    var state: TimerState
+    var time: ClosedRange<Date>
+    var kind: IntervalKind
+
+    init(
+        state: TimerState,
+        time: ClosedRange<Date>,
+        kind: IntervalKind
+    ) {
+        self.state = state
+        self.time = time
+        self.kind = kind
+    }
+}
