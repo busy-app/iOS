e80b400dd398b887483c4019cc48cd01f956ed22
Programistich <35292229+Programistich@users.noreply.github.com>
2025-03-26 21:04:29 +0200

    [Analytics] Support different organization by build type

--- BusyAnalytics/Services/FirebaseAnalyticsService.swift
+++ BusyAnalytics/Services/FirebaseAnalyticsService.swift
@@ -2,11 +2,9 @@ import FirebaseAnalytics
 
 final class FirebaseAnalyticsService: AnalyticsService {
     func track(event: any AnalyticsEvent) {
-        #if !DEBUG
         FirebaseAnalytics.Analytics.logEvent(
             event.key.rawValue,
             parameters: event.parameters()
         )
-        #endif
     }
 }
